2150e4627de458d51802be2ceef39e26a5ca6506
mwafak <mwafaqarbash3@gmail.com>
2019-05-11 19:19:39 +0200

    serialization added

--- passwort-authentication/app.js
+++ passwort-authentication/app.js
@@ -3,13 +3,34 @@ const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys").MongoURI;
+const flash = require("connect-flash");
+const session = require("express-session");
 //Ejs
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 
 //Bodyparser
 app.use(express.urlencoded({ extended: false }));
+//express session
 
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+//Connect flash
+app.use(flash());
+//Global vars
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  next();
+});
+
+//Connect flsh
+app.use(flash());
 //Connect to mongo
 mongoose
   .connect(db, { useNewUrlParser: true })
--- passwort-authentication/routes/users.js
+++ passwort-authentication/routes/users.js
@@ -49,8 +49,23 @@ router.post("/register", (req, res) => {
           email,
           password
         });
-        console.log(newUser);
-        res.send("hey");
+        // Hash  Password
+        bcrypt.genSalt(10, (err, salt) =>
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => {
+                req.flash(
+                  "success_msg",
+                  "you are now registered an can log in"
+                );
+                res.redirect("/users/login");
+              })
+              .catch(err => console.log(err));
+          })
+        );
       }
     });
   }
