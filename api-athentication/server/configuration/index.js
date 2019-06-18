563a88d4603bfb007e6d8d3b85f7e785c50f25be
mwafak <mwafaqarbash3@gmail.com>
2019-06-18 10:20:29 +0200

    gitignore file modifeid

--- api-athentication/server/configuration/index.js
+++ api-athentication/server/configuration/index.js
@@ -3,9 +3,8 @@ if (process.env.NODE_ENV === "test") {
     JWT_SECRET: "mwafak",
     oauth: {
       google: {
-        clientID:
-          "1080786384826-0t8qviq7l7c4mtc37kpi5a1r9hbsr52q.apps.googleusercontent.com",
-        clientSecret: "ymDRbzkoRTfTA2q5ruboEYt8"
+        clientID: "",
+        clientSecret: ""
       },
       facebook: {
         clientID: "number",
